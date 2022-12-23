b6fffbc38d5e65cfe46d97bc77130435720a14ef
yoongagagaga <yoongagagaga@gmail.com>
2022-12-23 22:17:31 +0900

    ADD : 22.12.19~ 3ch.operator

---
+++ src/operator_3/Exercise3_3.java
@@ -0,0 +1,10 @@
+package operator_3;
+
+public class Exercise3_3
+{
+    public static void main (String[] args)
+    {
+        int num = 10;
+        System.out.println( num > 0? "양수" : (num < 0 ? "음수" : "0" ));
+    }
+}
---
+++ src/operator_3/Exercise3_5.java
@@ -0,0 +1,10 @@
+package operator_3;
+
+public class Exercise3_5
+{
+    public static void main(String[] args)
+    {
+        int num = 333;
+        System.out.println(num - num%10 + 1);
+    }
+}
